cf5f82dad70fea6ad455ec7037a0a47cd9933878
xathon <36819668+xathon@users.noreply.github.com>
2019-12-02 02:51:50 +0100

    Day 1

---
+++ Day1/Day1Main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	T1 "github.com/xathon/AdventOfCodeGo/Day1/task1"
+	T2 "github.com/xathon/AdventOfCodeGo/Day1/task2"
+)
+
+func main() {
+	T1.Calc();
+	T2.Calc();
+}
---
+++ Day1/task1/Day1Task1.go
@@ -0,0 +1,40 @@
+package task1
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func Calc() {
+	file, err := os.Open("./Day1/input.txt")
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+	scanner := bufio.NewScanner(file);
+	scanner.Split(bufio.ScanLines);
+	var summe int
+	var zahl string
+	
+	for scanner.Scan() {
+		if zahl, err := strconv.Atoi(scanner.Text()); err == nil {
+			summe += getFuel(zahl);
+		}
+	}
+	_ = zahl;
+
+	file.Close();
+	fmt.Println("The answer to Puzzle 1 is: " + strconv.Itoa(summe));
+}
+
+func getFuel(mass int) int {
+	var fuel int
+	fuel = mass / 3;
+	fuel -= 2;
+	
+	return fuel
+	
+}
+
